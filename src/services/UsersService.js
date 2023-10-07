c045e49aacba32312c941fe1663295871bc923b1
Sheila Hernández <shey.hdez1999@gmail.com>
2023-10-07 10:31:21 +0200

    user service actualizdo

--- src/services/UsersService.js
+++ src/services/UsersService.js
@@ -3,3 +3,5 @@ import createHttp from './BaseService';
 const http = createHttp(true);
 
 export const getCurrentUser = () => http.get('/api/users/me');
+
+export const getUser = (id) => http.get(`/api/user/detail/${id}`);
